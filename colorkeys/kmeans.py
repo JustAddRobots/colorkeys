5e2024979016c3b7f25e48d833bf8345ec140fbe
JustAddRobots <justaddrobots@icloud.com>
2021-02-02 23:45:35 -0800

    ISSUE-004: Tested in venv, started palette layout fix

--- colorkeys/kmeans.py
+++ colorkeys/kmeans.py
@@ -4,7 +4,8 @@ import cv2
 import logging
 import numpy as np
 
-from matplotlib import pyplot
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
 from sklearn import cluster
 
 logger = logging.getLogger(__name__)
@@ -35,24 +36,37 @@ def get_cluster_histogram(clust):
     return hist
 
 
-def get_hist_bar(hist, clust):
-    hist_bar = np.zeros((50, 300, 3), dtype = "uint8")
+def get_hist_bar(hist, clust, img):
+    img_height, img_width, _ = img.shape
+    hist_bar_height = 60 #img_height
+    hist_bar = np.zeros((hist_bar_height, img_width, 3), dtype = "uint8")
     startX = 0
     for (percent, color) in zip(hist, clust.cluster_centers_):
-        endX = startX + (percent * 300)
-        cv2.rectangle(hist_bar, (int(startX), 0), (int(endX), 50),
+        endX = startX + (percent * img_width)
+        cv2.rectangle(hist_bar, (int(startX), 0), (int(endX), hist_bar_height),
             color.astype("uint8").tolist(), -1)
         startX = endX
     return hist_bar
 
 
 def plot_hist_bar(img, hist_bar):
-    pyplot.figure(figsize=(10,10))
-    pyplot.subplot(121)
-    pyplot.imshow(img)
-    pyplot.axis("off")
-    pyplot.subplot(122)
-    pyplot.imshow(hist_bar)
-    pyplot.axis("off")
-    pyplot.show()
+    img_height, img_width, _ = img.shape
+    canvas = plt.figure(
+        figsize = (12.80, 7.20), 
+        facecolor = "grey", 
+        tight_layout = True
+    )
+    spec = gridspec.GridSpec(ncols=1, nrows=2, figure=canvas)
+    screenshot = canvas.add_subplot(spec[0])
+    plt.axis("off")
+    palette = canvas.add_subplot(spec[1])
+    plt.axis("off")
+    screenshot.imshow(img, aspect="equal")
+    palette.imshow(hist_bar, aspect="equal")
+    plt.gca().set_box_aspect(img_height/img_width)
+    pos0 = palette.get_position()
+    pos1 = [pos0.x0, pos0.y0 - 0.18, pos0.width, pos0.height]
+    palette.set_position(pos1)
+    plt.show()
+
     return None
