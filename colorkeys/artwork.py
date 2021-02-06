4fc6a37a23da01ba5e5dc5e4976e2d16601f45d7
JustAddRobots <justaddrobots@icloud.com>
2021-02-06 15:38:37 -0800

    ISSUE-015: Added docstrings/comments

--- colorkeys/artwork.py
+++ colorkeys/artwork.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+This module facilitates creating an image matrix for data analysis.
+
+    Typical Usage:
+
+    my_artwork = Artwork("my_image_file.png")
+"""
+
 import cv2
 import errno
 import logging
@@ -9,8 +17,26 @@ logger = logging.getLogger(__name__)
 
 
 class Artwork:
+    """A class for loading image data and exposing underliying image properties.
+
+    Attributes:
+        filename (str): Image filename.
+        colorspace (str): Color space of image.
+        img (numpy.ndarray): Matrix of image data.
+        img_height (int): Height of image.
+        img_width (int): Width of image.
+        num_channels (int): Number of channels in image.
+    """
 
     def __init__(self, filename, **kwargs):
+        """Init Artwork.
+
+        Args:
+            filename(str): Image filename.
+
+        **kwargs:
+            colorspace (str): Color space of image.
+        """
         self._filename = self._get_filename(filename)
         self._colorspace = self._get_colorspace(kwargs)
         self._img = self._get_img()
@@ -46,11 +72,24 @@ class Artwork:
         "Image number of channels"
         return self._num_channels
 
-    @property
     def show_debug(self):
+        "Show class attribute debug information."
         return self._show_debug()
 
     def _get_filename(self, filename):
+        """Get filename.
+
+        Checks the existence of requested file.
+
+        Args:
+            filename (str): File name.
+
+        Returns:
+            filename (str): File name.
+
+        Raises:
+            FileNotFoundError: Requested file not found.
+        """
         if not os.path.exists(filename):
             raise FileNotFoundError(
                 errno.ENOENT,
@@ -60,12 +99,41 @@ class Artwork:
         return filename
 
     def _get_colorspace(self, kwargs):
+        """Get image color space.
+
+        An image is simply a matrix of data with no embedded color space information.
+        So it is not possible to detect the color space.
+
+        Args:
+            None
+
+        **kwargs:
+            colorspace (str): Color space of requested image.
+
+        Returns
+            colorspace (str): Color space of requested image.
+
+        Raises:
+            TypeError: colorspace not a string.
+        """
         colorspace = kwargs.setdefault("colorspace", "RGB")
         if not isinstance(colorspace, str):
             raise TypeError("colorspace must be a string")
         return colorspace
 
     def _get_img(self):
+        """Get image matrix for the requested color space.
+
+        cv2 automatically reads the image as BGR, so it must be converted to the
+        correct color space. For now, assume all images will be RBG. HSV will be
+        used for algorithm testing at a later date.
+
+        Args:
+            None
+
+        Returns:
+            img (numpy.ndarray): Image matrix.
+        """
         colorspace = self._colorspace
         if colorspace == "RGB":
             cnv = cv2.COLOR_BGR2RGB
