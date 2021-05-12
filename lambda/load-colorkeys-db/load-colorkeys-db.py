c1bd3a3e9083b3cc92a5be652bb261a0df3d265f
JustAddRobots <justaddrobots@icloud.com>
2021-05-12 14:29:58 -0700

    ISSUE-117: Added lambda, will need to mock locally

---
+++ lambda/load-colorkeys-db/load-colorkeys-db.py
@@ -0,0 +1,86 @@
+import boto3
+import io
+import logging
+import json
+import zipfile
+from decimal import Decimal
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def get_input_artifact(job, key):
+    logger.info(f"key: {key}")
+    s3_input = next(i for i in job["data"]["inputArtifacts"])["location"]["s3Location"]
+    bucket = s3_input["bucketName"]
+    objkey = s3_input["objectKey"]
+    obj = get_obj_from_s3zip(bucket, objkey, "imagedefinitions.json")
+    artifact = next(i[key] for i in obj if i["name"] == "colorkeys")
+    logger.info(f"artifact: {artifact}")
+    return artifact
+
+
+def get_user_param(job, key):
+    task_arn = job["data"]["actionConfiguration"]["configuration"]["UserParameters"]
+    logger.info(f"task_arn: {task_arn}")
+    return task_arn
+
+
+def get_obj_from_s3zip(bucket, objkey, internalfile):
+    s3 = boto3.resource("s3")
+    logger.info(f"bucket: {bucket}")
+    logger.info(f"objkey: {objkey}")
+    s3obj = s3.Object(bucket, objkey)
+    with zipfile.ZipFile(io.BytesIO(s3obj.get()["Body"].read())) as zf:
+        with zf.open(internalfile) as obj_json:
+            obj = json.loads(obj_json.read().decode(), parse_float=Decimal)
+    return obj
+
+
+def get_task_obj(task_hash):
+    bucket = "colorkeys-tmp"
+    internalfile = f"{task_hash[:8]}.colorkeys.json"
+    objkey = f"{internalfile}.zip"
+    obj = get_obj_from_s3zip(bucket, objkey, internalfile)
+    logger.info(f"obj: {obj}")
+    return obj
+
+
+def load_colorkeys(colorkeys):
+    db = boto3.resource("dynamodb")
+    tbl = db.Table("colorkeys-stage")
+    for colorkey in colorkeys:
+        for h in colorkey["histogram"]:
+            selector = (
+                f'{h["algo"]}#{h["colorspace"]}#{h["n_clusters"]}#'
+                f'{colorkey["cpu"]}#{colorkey["memory"]}#{colorkey["timestamp"]}'
+            )
+            logger.info(f"selector: {selector}")
+            colorkey["selector"] = selector
+            response = tbl.put_item(Item=colorkey)
+    return response
+
+
+def lambda_handler(event, context):
+    job = event["CodePipeline.job"]
+    logging.info(job)
+    cp = boto3.client("codepipeline")
+    try:
+        task_arn = get_user_param(job, "task_arn")
+        task_hash = task_arn.split("/")[-1]
+        colorkeys = get_task_obj(task_hash)
+        r = load_colorkeys(colorkeys)
+    except Exception as e:
+        logger.info("Lambda Failure")
+        logger.info(str(e))
+        cp.put_job_failure_result(
+            jobId = job["id"],
+            failureDetails = {"message": str(e), "type": "JobFailed"}
+        )
+    else:
+        logger.info(r)
+        cp.put_job_success_result(
+            jobId = job["id"],
+        )
+    logger.info("Lambda Complete")
+    return None
---
+++ lambda/run-fargate-ecs-colorkeys/run-fargate-ecs-colorkeys.py
@@ -0,0 +1,84 @@
+import boto3
+import io
+import logging
+import json
+import zipfile
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+FARGATE_CLUSTER = "workers"
+FARGATE_TASK_DEF = "colorkeys-deploy:12"
+FARGATE_SUBNET_ID = "subnet-2c7ff84a"
+FARGATE_SECURITY_GROUP_ID = "sg-0754b7137e6715e16"
+ARTIFACT_FILE = "imagedefinitions.json"
+
+
+def run_fargate_task(img):
+    ecs = boto3.client("ecs")
+    response = ecs.run_task(
+        cluster = FARGATE_CLUSTER,
+        count = 1,
+        launchType = "FARGATE",
+        networkConfiguration = {
+            "awsvpcConfiguration": {
+                "subnets": [
+                    FARGATE_SUBNET_ID,
+                ],
+                "securityGroups": [
+                    FARGATE_SECURITY_GROUP_ID,
+                ],
+                "assignPublicIp": "ENABLED"
+            }
+        },
+        taskDefinition = FARGATE_TASK_DEF
+    )
+    return response
+
+
+def get_input_artifact(job, key):
+    logger.info(f"key: {key}")
+    s3_input = next(i for i in job["data"]["inputArtifacts"])["location"]["s3Location"]
+    bucket = s3_input["bucketName"]
+    objkey = s3_input["objectKey"]
+    s3 = boto3.resource("s3")
+    logger.info(f"bucket: {bucket}")
+    logger.info(f"objkey: {objkey}")
+    s3obj = s3.Object(bucket, objkey)
+    with zipfile.ZipFile(io.BytesIO(s3obj.get()["Body"].read())) as zf:
+        with zf.open(ARTIFACT_FILE) as imgdef:
+            obj = json.loads(imgdef.read().decode())
+    artifact = next(i[key] for i in obj if i["name"] == "colorkeys")
+    logger.info(f"artifact: {artifact}")
+    return artifact
+
+
+def lambda_handler(event, context):
+    job = event["CodePipeline.job"]
+    cp = boto3.client("codepipeline")
+    try:
+        img = get_input_artifact(job, "imageUri")
+        r = run_fargate_task(img)
+        task_arn = r["tasks"][0]["taskArn"]
+        ecs = boto3.client("ecs")
+        waiter = ecs.get_waiter("tasks_stopped")
+        logger.info(f"Waiting for {task_arn}")
+        waiter.wait(cluster="workers", tasks=[task_arn])
+    except Exception as e:
+        logger.info("Lambda Failure")
+        logger.info(str(e))
+        cp.put_job_failure_result(
+            jobId = job["id"],
+            failureDetails = {"message": str(e), "type": "JobFailed"}
+        )
+    else:
+        logger.info(r)
+        cp.put_job_success_result(
+            jobId = job["id"],
+            outputVariables = {
+                "task_arn": task_arn
+            }
+        )
+    logger.info("Lambda Complete")
+    return None
