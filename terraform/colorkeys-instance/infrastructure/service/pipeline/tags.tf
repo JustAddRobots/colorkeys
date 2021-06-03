b6365fa9b6160e7302e39300db6ab9a3781d7609
JustAddRobots <justaddrobots@icloud.com>
2021-06-02 19:28:00 -0700

    ISSUE-127: Fixed vars/tags symlinks, buildspec for ECR_REPO

--- terraform/colorkeys-instance/infrastructure/service/pipeline/tags.tf
+++ terraform/colorkeys-instance/infrastructure/service/pipeline/tags.tf
@@ -1,11 +1,1 @@
-# stage_tags
-
-variable "default_tags" {
-  type  = map(string)
-  default = {
-    project     = "colorkeys"
-    environment = "stage"
-    terraform   = "true"
-    serverless  = "true"
-  }
-}
+../../../environment/stage/stage_tags.tf
--- terraform/colorkeys-instance/infrastructure/service/pipeline/vars.tf
+++ terraform/colorkeys-instance/infrastructure/service/pipeline/vars.tf
@@ -1,13 +1,1 @@
-# global
-
-variable "aws_region" {
-  default = "us-west-1"
-}
-
-variable "aws_profile" {
-  default = "terraform"
-}
-
-variable "project" {
-  default = "colorkeys"
-}
+../../../global/vars.tf
